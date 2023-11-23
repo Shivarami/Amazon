4634ddcd87867aac5124f8be1e15a6d91d561091
Sivaramreddy <sivaram27899@gmail.com>
2023-11-23 11:40:49 +0530

    modifiedindex

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+//const {onRequest} = require("firebase-functions/v2/https");
+//const logger = require("firebase-functions/logger");
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors')
